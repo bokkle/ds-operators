dc8fabfb0023d7dd1f3843dbaa18d93dfd2386e7
bokkle <mitch.r_@hotmail.com>
2023-09-16 19:18:49 -0400

    day 2

--- script.js
+++ script.js
@@ -22,87 +22,51 @@ const restaurant = {
       close: 24, // open 24h
     },
   },
-
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
-  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
-    console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
 };
 
-restaurant.orderDelivery({
-  time: '22:30',
-  address: 'Via del Sole, 21',
-  mainIndex: 2,
-  starterIndex: 2,
-});
+//reverse a string
+const reverse = (str) => {
+  const reversed = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed.push(str[i]);
+  }
+  return reversed.join('');
+};
+console.log(reverse('code'));
 
-const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+//take in string, add 1 after each character, except for spaces
+const addOnes = (str) => {
+  const result = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      result.push(`${str[i]}1`);
+    } else {
+      result.push(' ');
+    }
+  }
+  return result.join('');
+};
 
-const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(arr);
+//remove numbers from string
+const removeNums = (str) => {
+  const noNums = [];
+  const nums = '1234567890';
+  for (let i = 0; i < str.length; i++) {
+    if (isNaN(str[i])) {
+      noNums.push(str[i]);
+    }
+    if (str[i] === ' ') {
+      noNums.push(' ');
+    }
+  }
+  return noNums.join('');
+};
 
-//switching variables w/out destructuring
-// const temp = main
-// main = secondary
-// secondary = main
-// console.log(main, secondary)
-
-let [main, , secondary] = restaurant.categories;
-console.log(main, secondary); //italian vegetarian
-
-[main, secondary] = [secondary, main];
-console.log(main, secondary); //vegetarian italian
-
-// RECEIVE 2 RETURN VALUES FROM A FUNCTION
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
-
-// NESTED DESTRUCTURING
-const nested = [2, 4, [5, 6]];
-// const [i, , j] = nested;
-// console.log(i, j); // [2, [5, 6]]
-const [i, , [j, k]] = nested;
-console.log(i, j, k); // [2, 5, 6]
-
-// DEFAULT VALUES
-const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r);
-
-// DESTRUCTURING OBJECTS
-
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
-
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurantName, hours, tags);
-
-// DEFAULT VALS
-const { menu = [], starterMenu: starters = [] } = restaurant;
-console.log(menu, starters);
-
-// MUTATING VARIABLES
-let aa = 111;
-let bb = 999;
-const obj = { aa: 23, bb: 7, cc: 14 };
-
-({ aa, bb } = obj); // need to wrap in () because if you start a line with {} JS expects a code block
-console.log(aa, bb);
-
-// NESTED OBJS
-const {
-  fri: { open: o, close: cls },
-} = openingHours;
-console.log(`Open: ${o}`, `Close: ${cls}`);
+const countBy = (x, n) => {
+  const result = []
+  for (let i = x; i <= (n * x); i += x) {
+    result.push(i)
+  }
+  return result
+}
+console.log(countBy(10, 10))
